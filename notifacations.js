dd3b58566a13536dc145a292179b0a47b9960cc2
ineedtodosomething <141281958+ineedtodosomething@users.noreply.github.com>
2023-10-07 11:49:58 -0400

    Update notifacations.js

--- notifacations.js
+++ notifacations.js
@@ -2,5 +2,7 @@ console.log(Notification.permission);
 // if notifacations is deafault run code to ask for  permission
 if(Notification.permission == "default") {
     // ask permission for notifacations
-
+    Notification.requestPermission().then(permission => {
+        console.log(Notification);
+    });
 }
